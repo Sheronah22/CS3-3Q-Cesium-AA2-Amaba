eec384dae8c7beedde9bc593737063b8bd785d4d
sfaamaba-src <sfaamaba22@src.pshs.edu.ph>
2025-02-14 20:14:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function calculatePopulation() {
         return;
     }
 
-    let finalPop = Math.round(initialPopulation * Math.exp(growthRate * time));
+    let finalPopulation = Math.round(initialPopulation * Math.exp(growthRate * time));
     
     let region = prompt("Enter the monster’s location:");
     let monster = prompt("Enter the monster’s name:");
 
-    let fullName = (place + " " + monster).toUpperCase();
+    let fullName = (region + " " + monster).toUpperCase();
 
     document.getElementById("result").innerText = 
-        `After ${time} hours, the population of ${fullName} has increased to ${finalPop}!`;
+        `After ${time} hours, the population of ${fullName} has increased to ${finalPopulation}!`;
 }
