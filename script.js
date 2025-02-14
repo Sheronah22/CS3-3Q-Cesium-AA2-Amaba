103f3f9fb4ebf6f171e443c1fe9faecc38bf039c
sfaamaba-src <sfaamaba22@src.pshs.edu.ph>
2025-02-14 20:24:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-function calculatePopulation() {
-    let initialPopulation = parseFloat(prompt("Enter the starting number of monsters:"));
-    let growthRate = parseFloat(prompt("Enter the growth rate (0 to 1):"));
-    let time = parseFloat(prompt("Enter the number of hours:"));
-    
-    }
+function calculatePopulation() {  
+    let initialPopulation = parseFloat(prompt("Enter the starting number of monsters:"));  
+    let growthRate = parseFloat(prompt("Enter the growth rate (0 to 1):"));  
+    let time = parseFloat(prompt("Enter the number of hours:"));  
 
-    let finalPopulation = Math.round(initialPopulation * Math.exp(growthRate * time));
-    
-    let region = prompt("Enter the monster’s location:");
-    let monster = prompt("Enter the monster’s name:");
+    let finalPop = Math.round(initialPopulation * Math.exp(growthRate * time));  
 
-    let fullName = (region + " " + monster).toUpperCase();
+    let region = prompt("Enter the monster’s location:");  
+    let monster = prompt("Enter the monster’s name:");  
 
-    document.getElementById("result").innerText = 
-        `After ${time} hours, the population of ${fullName} has increased to ${finalPopulation}!`;
+    let fullName = (region + " " + monster).toUpperCase();  
+
+    document.getElementById("result").innerText =   
+        `After ${time} hours, the population of ${fullName} has increased to ${finalPop}!`;  
 }
