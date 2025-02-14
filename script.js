ff91b6fb133a6f3f00363ebb4102bd49958bbf70
sfaamaba-src <sfaamaba22@src.pshs.edu.ph>
2025-02-14 19:53:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 function calculatePopulation() {
-    let startPop = parseFloat(prompt("Enter the starting number of monsters:"));
-    let rate = parseFloat(prompt("Enter the growth rate (0 to 1):"));
+    let initialPopulation = parseFloat(prompt("Enter the starting number of monsters:"));
+    let growthRate = parseFloat(prompt("Enter the growth rate (0 to 1):"));
     let time = parseFloat(prompt("Enter the number of hours:"));
-
-    if (isNaN(startPop) || isNaN(rate) || isNaN(time)) {
-        alert("Please enter numbers only!");
+    
         return;
     }
 
-    let finalPop = Math.round(startPop * Math.exp(rate * time));
+    let finalPop = Math.round(startPop * Math.exp(growthRate * time));
     
     let place = prompt("Enter the monster’s location:");
     let monster = prompt("Enter the monster’s name:");
